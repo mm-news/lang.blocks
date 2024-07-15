d03828259b58eabf274b63c7ee3bd94046d9bc9a
mmtv <mmtv.news.tw@gmail.com>
2024-07-15 21:19:27 +0800

    THE FIRST WORD!
    definition:
    POS --> grammaticalSubcat --> word
    
     - Added words.js that contains all the words
     - Added grammaticalSubcat.js that contains all the grammatical subcategories
     - Added the first word!

---
+++ grammaticalSubcat.js
@@ -0,0 +1,51 @@
+// This file contains the functions that are used to generate the grammatical subcategories for the grammar rules.
+
+// Nouns subcategories
+
+// Verbs subcategories
+
+// Adjectives subcategories
+
+// Adverbs subcategories
+
+// Pronouns subcategories
+
+class PersonalPronoun extends Pronoun {
+  /**
+   * Personal pronouns are pronouns that are associated primarily with a particular person, in the grammatical sense.
+   * @param {String} subjective The subjective case of the pronoun
+   * @param {String} objective The objective case of the pronoun
+   * @param {Adjective} possessiveA The adjective possessive case of the pronoun
+   * @param {Noun} possessiveN The noun possessive case of the pronoun
+   * @param {String} reflexive The reflexive case of the pronoun
+   * @param {Number} person A number between 1 and 3 representing the person of the pronoun
+   * @param {Boolean} plural True for plural, False for singular
+   * @param {JSON} sentenceArgs The arguments for the sentence
+   */
+  constructor(
+    subjective,
+    objective,
+    possessiveA,
+    possessiveN,
+    reflexive,
+    person,
+    plural,
+    sentenceArgs
+  ) {
+    super(subjective, person, plural, sentenceArgs);
+    this.subjective = subjective;
+    this.objective = objective;
+    this.possessiveA = possessiveA;
+    this.possessiveN = possessiveN;
+    this.reflexive = reflexive;
+  }
+}
+// Prepositions subcategories
+
+// Conjunctions subcategories
+
+// Articles subcategories
+
+// Interjections subcategories
+
+// Helper functions
---
+++ words.js
@@ -0,0 +1,24 @@
+// This file contains all the word classes
+// Nouns
+
+// Verbs
+
+// Adjectives
+
+// Adverbs
+
+// Pronouns
+class I extends PersonalPronoun {
+  constructor(sentenceArgs) {
+    super("I", "me", "my", "mine", "myself", 1, false, sentenceArgs); //TODO: The type here now isn't correct yet
+  }
+}
+// Prepositions
+
+// Conjunctions
+
+// Articles
+
+// Interjections
+
+// Helper functions
